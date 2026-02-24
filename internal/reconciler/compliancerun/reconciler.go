9f5c935070cd81e4b0608bd0dcd92097ccc78bef
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-02-24 17:37:15 +0200

    Adapt ComplianceRun reconciliation

--- internal/reconciler/compliancerun/reconciler.go
+++ internal/reconciler/compliancerun/reconciler.go
@@ -8,12 +8,15 @@ import (
 	"context"
 	"fmt"
 
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/client-go/rest"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 
 	v1alpha1config "github.com/gardener/diki-operator/pkg/apis/config/v1alpha1"
+	"github.com/gardener/diki-operator/pkg/apis/diki/v1alpha1"
 )
 
 // Reconciler reconciles compliance runs.
@@ -27,6 +30,37 @@ type Reconciler struct {
 func (r *Reconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := logf.FromContext(ctx)
 
-	log.Info(fmt.Sprintf("Reconciling ComplianceRun %s", req.Name))
+	complianceRun := &v1alpha1.ComplianceRun{}
+
+	if err := r.Client.Get(ctx, client.ObjectKey{Name: req.Name}, complianceRun); err != nil {
+		if apierrors.IsNotFound(err) {
+			log.Info("Object is gone, stop reconciling")
+			return reconcile.Result{}, nil
+		}
+		return reconcile.Result{}, fmt.Errorf("error retrieving complianceRun: %w", err)
+	}
+
+	if len(complianceRun.Status.Phase) > 0 {
+		log.Info("ComplianceRun already processed, stop reconciling", "name", complianceRun.Name, "phase", complianceRun.Status.Phase)
+		return reconcile.Result{}, nil
+	}
+
+	// Update phase to Running
+	patch := client.MergeFrom(complianceRun.DeepCopy())
+	// TODO(AleksandarSavchev): Update conditions here.
+	complianceRun.Status.Phase = v1alpha1.ComplianceRunRunning
+	if err := r.Client.Status().Patch(ctx, complianceRun, patch); err != nil {
+		return reconcile.Result{}, r.handleFailedRun(ctx, complianceRun, err)
+	}
+
+	log.Info("Updated ComplianceRun phase to Running", "name", complianceRun.Name)
+
+	dikiConfig, err := r.deployDikiConfigMap(ctx, complianceRun)
+	if err != nil {
+		return reconcile.Result{}, r.handleFailedRun(ctx, complianceRun, err)
+	}
+
+	log.Info(fmt.Sprintf("Created ConfigMap %s/%s", dikiConfig.Namespace, dikiConfig.Name))
+
 	return ctrl.Result{}, nil
 }
