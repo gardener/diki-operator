317ebbebdf7c99956bc1e8c2652d239500088cb5
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-03-13 17:31:18 +0200

    Add Output interface

---
+++ internal/component/dikiexporter/outputs/output.go
@@ -0,0 +1,19 @@
+// SPDX-FileCopyrightText: SAP SE or an SAP affiliate company and Gardener contributors
+//
+// SPDX-License-Identifier: Apache-2.0
+
+package outputs
+
+import (
+	"context"
+
+	dikireport "github.com/gardener/diki/pkg/report"
+
+	"github.com/gardener/diki-operator/pkg/apis/dikiexporter/v1alpha1"
+)
+
+// Output is the interface for exporting Diki reports to different output types.
+type Output interface {
+	Type() v1alpha1.OutputType
+	Export(ctx context.Context, report dikireport.Report) (any, error)
+}
