187f4b83bc410a13e2ec39f210c41381464aad2c
Aleksandar Savchev <aleksandar.savchev@sap.com>
2026-08-17 11:02:09 +0300

    Add OutputWebhook API

--- pkg/apis/diki/v1alpha1/types_reportoutput.go
+++ pkg/apis/diki/v1alpha1/types_reportoutput.go
@@ -43,6 +43,9 @@ type Output struct {
 	// ConfigMap contains the configuration for exporting the report to a ConfigMap.
 	// +optional
 	ConfigMap *OutputConfigMap `json:"configMap,omitempty"`
+	// Webhook contains the configuration for exporting the report via an HTTP webhook.
+	// +optional
+	Webhook *OutputWebhook `json:"webhook,omitempty"`
 }
 
 // OutputConfigMap contains the configuration for exporting the report to a ConfigMap.
@@ -55,4 +58,42 @@ type OutputConfigMap struct {
 	// Defaults to "compliance-scan-report-".
 	// +kubebuilder:default="compliance-scan-report-"
 	NamePrefix string `json:"namePrefix,omitempty"`
+}
+
+// OutputWebhook contains the configuration for exporting the report via an HTTP webhook.
+type OutputWebhook struct {
+	// URL is the destination endpoint to which the report will be POSTed.
+	URL string `json:"url"`
+	// CredentialsRef is a reference to a Secret whose data at the given key contains a JSON object
+	// where keys are HTTP header names and values are the corresponding header values
+	// to include in the webhook request.
+	// +optional
+	CredentialsRef *SecretReference `json:"credentialsRef,omitempty"`
+	// TLS configures TLS settings for the webhook connection.
+	// Only relevant when URL uses the HTTPS scheme.
+	// +optional
+	TLS *TLSConfig `json:"tls,omitempty"`
+}
+
+// SecretReference is a reference to a specific key within a Secret.
+type SecretReference struct {
+	// Name is the name of the Secret.
+	Name string `json:"name"`
+	// Namespace is the namespace of the Secret.
+	Namespace string `json:"namespace"`
+	// Key is the key within the Secret's data.
+	// +optional
+	Key *string `json:"key,omitempty"`
+}
+
+// TLSConfig configures TLS settings for output types that make outbound HTTPS connections.
+type TLSConfig struct {
+	// InsecureSkipVerify disables TLS certificate verification.
+	// Use with caution; intended for development/testing environments.
+	InsecureSkipVerify bool `json:"insecureSkipVerify"`
+	// CASecretRef is a reference to a Secret containing a custom CA certificate bundle.
+	// The referenced key should contain PEM-encoded CA certificate(s).
+	// If not set, the system's root CA pool is used.
+	// +optional
+	CASecretRef *SecretReference `json:"caSecretRef,omitempty"`
 }
